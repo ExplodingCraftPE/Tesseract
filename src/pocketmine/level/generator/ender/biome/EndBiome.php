8ac0035f86778661dc7a7635397d2fe2864a7a21
Apple <ytapplegamer@gmail.com>
2017-06-20 01:33:50 -0400

    End files didn't upload

---
+++ src/pocketmine/level/generator/ender/biome/EndBiome.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace pocketmine\level\generator\ender;
+
+use pocketmine\level\generator\biome\Biome;
+
+class EndBiome extends Biome{
+
+	public function __construct(){
+	}
+
+	public function getName(){
+		return "EndBiome";
+	}
+}
